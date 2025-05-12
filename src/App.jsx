cadc1bf809f5cdd515ed7306cd5300d21bcaba3d
SK0603 <khalid.hamid2003@gmail.com~>
2025-05-12 12:21:51 +0300

    added movie details file and edited the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from '/components/Navbar';
-import Footer from '/components/Footer';
-import Home from '/pages/Home';
-import Movies from '/pages/Movies';
-import MovieDetails from '/pages/MovieDetails';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
