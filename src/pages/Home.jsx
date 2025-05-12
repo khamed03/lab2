78ac7814e75ed6519c11715f5b32f705a5d672c3
SK0603 <khalid.hamid2003@gmail.com~>
2025-05-12 12:38:14 +0300

    edited the errors and finished the code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 function Home() {
   return (
     <div className="text-center">
-      <h1>Welcome to Movie Explorer 🎬</h1>
-      <img src="https://via.placeholder.com/600x300" alt="Welcome" className="img-fluid mt-3" />
+      <h1>Welcome to Movie Explorer</h1>
+      <img src="https://plus.unsplash.com/premium_photo-1710961232986-36cead00da3c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW92aWVzfGVufDB8fDB8fHww"
+ alt="Welcome" className="img-fluid mt-3" />
     </div>
   );
 }
