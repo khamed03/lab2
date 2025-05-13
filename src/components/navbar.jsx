45cdd44c6afe37d1117a7ce25773436830d6557d
SK0603 <khalid.hamid2003@gmail.com~>
2025-05-13 15:38:31 +0300

    testing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function AppNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/">Movie Explorer</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Movie database</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav>
